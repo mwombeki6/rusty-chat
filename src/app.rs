55734339cb4baa6b09e602c0b86c0de662aff050
mwombeki6 <hackerlubex@protonmail.com>
2023-08-22 21:29:05 +0300

    remove HomePage() function in app.rs

--- src/app.rs
+++ src/app.rs
@@ -26,18 +26,3 @@ pub fn App(cx: Scope) -> impl IntoView {
         </Router>
     }
 }
-
-/// Renders the home page of your application.
-#[component]
-fn HomePage(cx: Scope) -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
-    view! { cx,
-        <h1>"Welcome to Rusty chat!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
-    }
-}
-
-
