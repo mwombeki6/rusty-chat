41f0135586a602f9bca2ffc176b1317e12fd026a
mwombeki6 <hackerlubex@protonmail.com>
2023-08-22 23:18:50 +0300

    commit

--- src/model/conversation.rs
+++ src/model/conversation.rs
@@ -1,9 +1,17 @@
-use serde:: Deserialize;
+use serde::Deserialize;
 use serde::Serialize;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Conversation {
-    pub message: Vec<Message>
+    pub message: Vec<Message>,
+}
+
+impl Conversation {
+    pub fn new() -> Conversation {
+        Conversation {
+            message: Vec::new(),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
