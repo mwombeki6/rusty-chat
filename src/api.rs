e07487465d20c74e34633ec2fffc140cc98309fb
mwombeki6 <hackerlubex@protonmail.com>
2023-08-23 21:38:43 +0300

    commit

--- src/api.rs
+++ src/api.rs
@@ -7,4 +7,11 @@ pub async fn converse(cx: Scope, prompt: Conversation) -> Result<String, ServerF
     use leptos_actix::extract;
     use actix_web::web::Data;
     use actix_web::dev::ConnectionInfo;
+
+    let model = extract(cx, |data: Data<Llama>, _connection: ConnectionInfo| async {
+        data.into_inner();
+    })
+    .await.unwrap();
+    
+    use llm::KnownModel;
 }
