14aa07333eb2011e2c501960025a6de3b5d6062b
mwombeki6 <hackerlubex@protonmail.com>
2023-08-23 21:26:20 +0300

    commit api.rs

---
+++ src/api.rs
@@ -0,0 +1,6 @@
+use leptos::*;
+use crate::model::conversation::Conversation;
+
+#[server(Converse "/api")]
+pub async fn converse(cx: Scope, prompt: Conversation) -> Result<String, ServerFnError> {
+}
