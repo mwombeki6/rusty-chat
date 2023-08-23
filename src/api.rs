63f56424096b84595194fdc8f1694340091dcd63
mwombeki6 <hackerlubex@protonmail.com>
2023-08-23 21:36:44 +0300

    commit

--- src/api.rs
+++ src/api.rs
@@ -3,4 +3,8 @@ use crate::model::conversation::Conversation;
 
 #[server(Converse "/api")]
 pub async fn converse(cx: Scope, prompt: Conversation) -> Result<String, ServerFnError> {
+    use llm::models::Llama;
+    use leptos_actix::extract;
+    use actix_web::web::Data;
+    use actix_web::dev::ConnectionInfo;
 }
