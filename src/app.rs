a153b4eb3997e06734e89822b6fa521087fa5615
mwombeki6 <hackerlubex@protonmail.com>
2023-08-23 21:12:55 +0300

    add create_signal

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,15 @@
 use leptos::*;
 use leptos_meta::*;
+
+use crate::model::conversation::Conversation;
 //use leptos_router::*;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context(cx);
+
+    let (conversation, set_conversation) = create_signal(cx, Conversation::new());
 
     view! { cx,
         // injects a stylesheet into the document <head>
