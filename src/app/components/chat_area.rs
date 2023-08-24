a10e0546bd02f2f1221887242a625d946b9851f6
mwombeki6 <hackerlubex@protonmail.com>
2023-08-24 22:00:47 +0300

    commit

--- src/app/components/chat_area.rs
+++ src/app/components/chat_area.rs
@@ -1,9 +1,9 @@
-use leptos::{*, html::Div};
+use leptos::{html::Div, *};
 
 use crate::model::conversation::Conversation;
 
-const USER_MESSAGE_DARK_MODE_COLORS: &str  = "bg-blue-500 text-white";
-const USER_MESSAGE_LIGHT_MODE_COLORS: &str  = "bg-blue-700 text-white";
+const USER_MESSAGE_DARK_MODE_COLORS: &str = "bg-blue-500 text-white";
+const USER_MESSAGE_LIGHT_MODE_COLORS: &str = "bg-blue-700 text-white";
 const USER_MESSAGE_CLASS: &str = "max-w-md p-4 mb-5 rounded-lg self-end";
 
 const MODEL_MESSAGE_LIGHT_MODE_COLORS: &str = "bg-gray-200 text-black";
@@ -16,51 +16,52 @@ const CHAT_AREA_DARK_MODE_COLORS: &str = "border-zinc-700 bg-zinc-900";
 
 #[component]
 pub fn ChatArea(conversation: ReadSignal<Conversation>) -> impl IntoView {
-    let dark_mode = use_context::<ReadSignal<bool>>().expect("should be able to get dark mode state");
+    let dark_mode =
+        use_context::<ReadSignal<bool>>().expect("should be able to get dark mode state");
 
     let user_message_class = Signal::derive(move || {
-      if dark_mode.get() {
-        format!("{USER_MESSAGE_CLASS} {USER_MESSAGE_DARK_MODE_COLORS}")
-      } else {
-        format!("{USER_MESSAGE_CLASS} {USER_MESSAGE_LIGHT_MODE_COLORS}")
-      }
+        if dark_mode.get() {
+            format!("{USER_MESSAGE_CLASS} {USER_MESSAGE_DARK_MODE_COLORS}")
+        } else {
+            format!("{USER_MESSAGE_CLASS} {USER_MESSAGE_LIGHT_MODE_COLORS}")
+        }
     });
 
     let model_message_class = Signal::derive(move || {
-      if dark_mode.get() {
-        format!("{MODEL_MESSAGE_CLASS} {MODEL_MESSAGE_DARK_MODE_COLORS}")
-      } else {
-        format!("{MODEL_MESSAGE_CLASS} {MODEL_MESSAGE_LIGHT_MODE_COLORS}")
-      }
+        if dark_mode.get() {
+            format!("{MODEL_MESSAGE_CLASS} {MODEL_MESSAGE_DARK_MODE_COLORS}")
+        } else {
+            format!("{MODEL_MESSAGE_CLASS} {MODEL_MESSAGE_LIGHT_MODE_COLORS}")
+        }
     });
 
     let chat_div_ref = create_node_ref::<Div>();
     create_effect(move |_| {
-      conversation.get();
-      if let Some(div) = chat_div_ref.get() {
-        div.set_scroll_top(div.scroll_height());
-      }
+        conversation.get();
+        if let Some(div) = chat_div_ref.get() {
+            div.set_scroll_top(div.scroll_height());
+        }
     });
 
-  let chat_area_class = Signal::derive(move || {
-    if dark_mode.get() {
-      format!("{CHAT_AREA_CLASS} {CHAT_AREA_DARK_MODE_COLORS}")
-    } else {
-      format!("{CHAT_AREA_CLASS} {CHAT_AREA_LIGHT_MODE_COLORS}")
-    }
-  });
-
-  view! {
-        <div class={chat_area_class.get()} node_ref=chat_div_ref>
-        {move || conversation.get().messages.iter().map(move |message| {
-            let class_str = if message.user { user_message_class.get() } else { model_message_class.get()};
-            view! {cx,
-              <div class={class_str}>
-                {message.text.clone()}
-              </div>
-            }
-          }).collect::<Vec<_>>()
+    let chat_area_class = Signal::derive(move || {
+        if dark_mode.get() {
+            format!("{CHAT_AREA_CLASS} {CHAT_AREA_DARK_MODE_COLORS}")
+        } else {
+            format!("{CHAT_AREA_CLASS} {CHAT_AREA_LIGHT_MODE_COLORS}")
         }
-      </div>
-  }
+    });
+
+    view! {
+          <div class={chat_area_class.get()} node_ref=chat_div_ref>
+          {move || conversation.get().messages.iter().map(move |message| {
+              let class_str = if message.user { user_message_class.get() } else { model_message_class.get()};
+              view! {cx,
+                <div class={class_str}>
+                  {message.text.clone()}
+                </div>
+              }
+            }).collect::<Vec<_>>()
+          }
+        </div>
+    }
 }
